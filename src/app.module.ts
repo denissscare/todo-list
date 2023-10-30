bee1b3c542bb47f2b6014301409556bea8ced972
denissscare <denissscare@gmail.com>
2023-10-30 20:03:35 +0800

    feat: add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+import { User } from './user/user.model';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const config: SqliteConnectionOptions = {
+  type: 'sqlite',
+  database: './db/sqlite.db',
+  entities: [User],
+  synchronize: true,
+};
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRoot(config),
+    UserModule,
   ],
   controllers: [],
   providers: [],
