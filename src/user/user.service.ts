d06a3e16bed2b90af6e68667f3a56fb933aa86c4
denissscare <denissscare@gmail.com>
2023-11-01 18:14:27 +0800

    add CRUD operation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entity/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userRepository.create(dto);
+    await UserEntity.save(user);
+    return user;
+  }
+
+  async getAllUsers() {
+    return await this.userRepository.find();
+  }
+
+  async getUserByEmail(email: string) {
+    return await this.userRepository.findOne({
+      where: { email },
+    });
+  }
+
+  async deleteUser(id: number) {
+    return await this.userRepository.delete(id);
+  }
+}
