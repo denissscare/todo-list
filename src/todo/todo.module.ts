6ec205ca84bc6ebac01b75594aadb24e75f103c4
denissscare <denissscare@gmail.com>
2023-11-03 18:19:44 +0800

    update todo.module. add imports

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoEntity } from './entity/todo.entity';
+import { TaskEntity } from '../task/entity/task.entity';
+import { UserEntity } from '../user/entity/user.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
   controllers: [TodoController],
   providers: [TodoService],
+  imports: [
+    UserModule,
+    TypeOrmModule.forFeature([TodoEntity, TaskEntity, UserEntity]),
+  ],
 })
 export class TodoModule {}
