62d64469d76f5f443e72264c733210fbd56fdc1d
denissscare <denissscare@gmail.com>
2023-11-03 18:20:50 +0800

    add getAllTodo service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TodoEntity } from './entity/todo.entity';
+import { Repository } from 'typeorm';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+    private readonly userService: UserService,
+  ) {}
+
+  async getAllTodo(): Promise<any> {
+    return await this.todoRepository.find({
+      relations: ['tasks', 'owner'],
+    });
+  }
+}
